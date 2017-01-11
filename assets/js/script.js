d3290ae8b89eca6563b8beda09f9d269ad57bdf4
Ziqi Chen <ziqichen6@berkeley.edu>
2017-01-10 23:51:41 -0800

    tic tac toe

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,103 @@ $(document).ready(function() {
     cycleItems();
   });
 
+
+
+
+  // TIc tac toe
+  var winner;
+  var currentPlayer = 1;
+  $(".start-button").click( function() {
+    currentPlayer = 1;
+    $(".square").text("");
+    // document.getElementById("big-title").removeChild(document.getElementById("congratulations"));
+    $("#congratulations").remove();
+    playGame();   
+  });
+
+  function playGame() {
+    $(".square").off('click').click( function() {
+      if (currentPlayer == 1) {
+        this.textContent = 'x';
+        if(checkWinner()) {
+          var congratulations = document.createElement('div');
+          congratulations.textContent = "Congratulations to " +  currentPlayer + " !";
+          congratulations.id = "congratulations";
+          document.getElementById("big-title").appendChild(congratulations); 
+        }
+        currentPlayer = 2;
+      } else {
+        this.textContent = 'o';
+        if(checkWinner()) {
+          var congratulations = document.createElement('div');
+          congratulations.textContent = "Congratulations to " +  currentPlayer + " !";
+          congratulations.id = "congratulations";
+          document.getElementById("big-title").appendChild(congratulations);
+        }
+        currentPlayer = 1;
+      }
+    });
+  }
+
+  function checkWinner() {
+    var arr = [];
+    $(".row").each(function(i,rrow){
+      var squares = [];
+      console.log(rrow);
+
+
+      $(rrow).children().each(function(i,content){
+        squares.push(content.textContent);
+      })
+      arr.push(squares);
+    });
+
+    for(let row = 0; row < arr.length; row++) {
+      let firstSquare = arr[row][0];
+      for(let col = 1; col < arr[row].length; col++) {
+        if((!firstSquare) || (arr[row][col] != firstSquare)) {
+          break;
+        } else if (col == arr[row].length-1) {
+          console.log('1');
+          return true;
+        }
+      }
+    }
+
+    for(let col = 0; col < arr[0].length; col++) {
+      let firstSquare = arr[0][col];
+      for(let row = 1; row < arr.length; row++) {
+        if((!firstSquare) || (arr[row][col] != firstSquare)) {
+          break;
+        } else if (row == arr.length-1) {
+          console.log('2');
+          return true;
+        }
+      }
+    }
+
+    for(let row = 0; row < arr.length; row++) {
+      let firstSquare = arr[0][0];
+      if ((!firstSquare)||(arr[row][row] != firstSquare)) {
+        break;
+      } else if (row == arr.length-1) {
+        console.log('3');
+        return true;
+      }
+    }
+
+
+    for(let row = 0; row < arr.length; row++) {
+      let firstSquare = arr[0][arr.length-1];
+      if((!firstSquare) || (arr[row][arr.length-1-row] != firstSquare)) {
+        break;
+      } else if (row == arr.length-1) {
+        return true;
+      }     
+    }  
+    return false;
+  }
+
   // Dropdown Menu
   // $('#navigation-coding').hover(function(){ 
   //     $('.coding-dropdown-hover').show();
